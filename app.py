1d6341f075456ae230af5eece8ca2aa6bbb085d5
sunyy <sunyy@xxx.com>
2020-06-05 09:47:26 +0800

    modify port

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello Discovery!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
